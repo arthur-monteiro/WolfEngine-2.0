53df41a932e8c5ff56532883577165dffdfe35fb
Arthur MONTEIRO <atou.monteiro@gmail.com>
2026-03-06 12:00:21 +0100

    fix a validation error with android emulator

--- GraphicAPIBroker/Private/Vulkan/SwapChainVulkan.cpp
+++ GraphicAPIBroker/Private/Vulkan/SwapChainVulkan.cpp
@@ -137,7 +137,10 @@ void Wolf::SwapChainVulkan::initialize(const SwapChainCreateInfo& swapChainCreat
 	createInfo.imageColorSpace = surfaceFormat.colorSpace;
 	createInfo.imageExtent = extent;
 	createInfo.imageArrayLayers = 1;
-	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+#ifndef __ANDROID__
+	createInfo.imageUsage |= VK_IMAGE_USAGE_STORAGE_BIT;
+#endif
 
 	const QueueFamilyIndices& indices = g_vulkanInstance->getQueueFamilyIndices();
 	const uint32_t queueFamilyIndices[] = { static_cast<uint32_t>(indices.graphicsFamily), static_cast<uint32_t>(indices.presentFamily) };
